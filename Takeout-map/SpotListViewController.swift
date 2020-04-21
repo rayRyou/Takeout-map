5a4c567973998a0f4ef35e852252fc1deb23f1b2
kiriem <kirie0513@gmail.com>
2020-04-21 21:42:28 +0900

    SpotListViewControllerの status bar デザインを修正

--- Takeout-map/SpotListViewController.swift
+++ Takeout-map/SpotListViewController.swift
@@ -1,52 +1,68 @@
 //
-//  ShopListTableaViewController.swift
-//  takeout-map
+//  SpotListViewController.swift
+//  Takeout-map
 //
-//  Created by Ryou on 2020/04/05.
+//  Created by Kirie Miyajima on 2020/04/21.
 //  Copyright © 2020 Ryou. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class SpotListTableaViewController: UITableViewController {
+class SpotListViewController:UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+    
     var searchedSpotArray:Array<Spot>?
     let searchBar = UISearchBar()
+    
+    let spotListTableView:UITableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = Common.LightColor
-
+        
+        let width = self.view.frame.width
+        let height = self.view.frame.height
+                
+        //table view settings
+        spotListTableView.frame = CGRect(x: 0, y: 44, width: width, height: height-44)
+        spotListTableView.delegate = self
+        spotListTableView.dataSource = self
+        spotListTableView.backgroundColor = Common.LightColor
+        
+        self.view.addSubview(spotListTableView)
+            
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if self.searchedSpotArray != nil {
             return self.searchedSpotArray?.count ?? 0
         }else{
             return DataManager.shared.spotArray.count
         }
-        
     }
-
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+    
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 120.0
     }
-    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+    
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         searchBar.placeholder = "検索ワードを入力してください"
         searchBar.delegate = self
         searchBar.backgroundImage = UIImage()
         searchBar.tintColor = Common.LightColor
+        searchBar.backgroundColor = Common.LightColor
         searchBar.searchTextField.backgroundColor = .systemBackground
         return searchBar
     }
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCell(withIdentifier: "spot-cell")
 
         if cell == nil {
@@ -72,7 +88,7 @@ class SpotListTableaViewController: UITableViewController {
         return cell!
     }
 
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         var spot = DataManager.shared.spotArray[indexPath.row]
         if self.searchedSpotArray?.count ?? 0 > 0 {
             spot = self.searchedSpotArray![indexPath.row]
@@ -82,21 +98,26 @@ class SpotListTableaViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
 
     }
-    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
         self.searchBar.resignFirstResponder()
     }
-}
-extension SpotListTableaViewController:UISearchBarDelegate {
+    
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        var resultArray:[Spot] = DataManager.searchArray(searchText: searchText)
-        if searchText.count < 1 {
-            self.searchedSpotArray = nil
-        }else{
-            self.searchedSpotArray = resultArray
-        }
-        self.tableView.reloadData()
-    }
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.resignFirstResponder()
-    }
+           var resultArray:[Spot] = DataManager.searchArray(searchText: searchText)
+           if searchText.count < 1 {
+               self.searchedSpotArray = nil
+           }else{
+               self.searchedSpotArray = resultArray
+           }
+           self.spotListTableView.reloadData()
+       }
+       func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+           searchBar.resignFirstResponder()
+       }
+    
+    
+    
 }
+
+
